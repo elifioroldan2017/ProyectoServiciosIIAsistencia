56da3982d49d42793f01f70718cdcaaf12582d0c
elifio <elifioroldan@hotmail.com>
2023-10-09 18:58:27 -0500

    Crear componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPrincipalComponent } from './login/login-principal/login-principal.component';
+import { FormLoginComponent } from './login/form-login/form-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPrincipalComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
