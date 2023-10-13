9fe220e9a9c0f9b0e9375a470405ef5a9410595c
IRTP\eroldan <eroldan@irtp.gob.pe>
2023-10-13 17:25:14 -0500

    Seccion

--- src/app/seccion/tabla-seccion/tabla-seccion.component.ts
+++ src/app/seccion/tabla-seccion/tabla-seccion.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SeccionModule } from '../seccion.module';
 import { Seccion } from '../interface/Seccion';
 import { SeccionService } from '../seccion.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabla-seccion',
@@ -10,7 +11,7 @@ import { SeccionService } from '../seccion.service';
 })
 export class TablaSeccionComponent {
 
-  constructor(private seccionService:SeccionService){
+  constructor(private seccionService:SeccionService,private router:Router){
 
   }
   get secciones():Seccion[]{
@@ -18,6 +19,7 @@ export class TablaSeccionComponent {
   }
 
   editar(idseccion:number){
+    this.router.navigate(["seccion/editar/"+idseccion])
 
   }
 
