ab0f5c3b76c8743343bb8ff87505c5e399006998
elifio <elifioroldan@hotmail.com>
2023-11-27 22:00:36 -0500

    Login

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { LoginService } from 'src/app/login/login.service';
+import { UsuarioService } from 'src/app/usuario/usuario.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  nombreCompleto:string=""
 
+  constructor(private loginService:LoginService,private usuarioService:UsuarioService, private router:Router)
+  {
+  
+    var data = this.usuarioService.obtenerUsuarioDesdeStorage()
+    this.nombreCompleto= data.name+" "+data.lastname1+" "+data.lastname2
+  }
+
+  cerrarSesion(){
+    this.usuarioService.eliminarUsuarioDelStorage();
+    this.loginService.oUser=null
+    this.router.navigate(["/"])
+  }
+
+  
 }
